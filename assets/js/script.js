a80d8c977fca59f39ddd4e186c68de3459e1607b
Nivdek <>
2023-10-20 15:19:45 +0000

    changed entire gameplay mechanic because could not get first to work

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,45 +1,55 @@
 /** User Input script */
-let buttons = document.getElementsByTagName("button");
-let userPlay = document.getElementById("user-play");
-let cpuPlay = document.getElementById("cpu-play");
-let userValue = "";
-let gameText = document.getElementsByClassName("game-text");
+const buttons = document.querySelectorAll(".btn");
+let userImage = document.getElementById("user-play").src;
+let cpuImage = document.getElementById("cpu-play").src;
+let gameText = document.getElementById("game-text");
+let player;
+let cpu;
+let result;
 
-buttons[0].addEventListener("click", rockSelect);
-buttons[1].addEventListener("click", paperSelect);
-buttons[2].addEventListener("click", scissorsSelect);
+buttons.forEach(button => button.addEventListener("click", () => {
+    player = button.querySelector('p').textContent;
+    userImage = button.querySelector('img').src;
+    document.getElementById("user-play").src = userImage;
+    cpuTurn()
+    gameText.textContent  = checkWinner();
+}));
 
-//User input and value determiner
-function rockSelect() {
-    userPlay.src = "assets/images/rock.jpg"
-    userValue = "R"
+function cpuTurn (){
+    let randomNumber = Math.floor(Math.random() * 3) + 1;
+    switch(randomNumber){
+        case 1:
+            cpu = "Rock";
+            break;
+        case 2:
+            cpu = "Paper";
+            break;
+        case 3:
+            cpu = "Scissors";
+            break;
+    }
+
+    if (cpu === "Rock") {
+        cpuImage = "assets/images/rock.jpg"
+        document.getElementById("cpu-play").src = cpuImage;
+    } else if (cpu === "Paper") {
+        cpuImage = "assets/images/paper.jpg"
+        document.getElementById("cpu-play").src = cpuImage;
+    } else if (cpu === "Scissors") {
+        cpuImage = "assets/images/scissors.jpg"
+        document.getElementById("cpu-play").src = cpuImage;
+    }
 }
-function paperSelect() {
-    userPlay.src = "assets/images/paper.jpg"
-    userValue = "P";
+
+function checkWinner() {
+    if(player == cpu) {
+        return "Stalemate!"
+    } else if(cpu == "Rock") {
+        return (player == "Paper") ? "Victory!" : "Defeat!";
+    } else if (cpu == "Paper") {
+        return (player == "Scissors") ? "Victory!" : "Defeat!";
+    } else if (cpu == "Scissors") {
+        return (player == "Rock") ? "Victory!" : "Defeat!";
+    }
 }
-function scissorsSelect() {
-    userPlay.src = "assets/images/scissors.jpg"
-    userValue = "S"
-}
 
-// Computer Input and value determiner
-let cpuImages = ["assets/images/rock.jpg", "assets/images/paper.jpg", "assets/images/scissors.jpg"];
-let randomNumber = Math.floor(Math.random() *  3);
-cpuPlay.src = cpuImages[randomNumber];
-let cpuValue = ["R", "P", "S"][randomNumber];
-
-// Score Evaluation 
-let outcomes = {
-    RR: "Stalemate!",
-    RP: "Defeat!",
-    RS: "Victory!",
-    PR: "Victory!",
-    PP: "Stalemate!",
-    PS: "Defeat!",
-    SR: "Defeat!",
-    SP: "Victory!",
-    SS: "Stalemate!",
-};
-
-let outcomeValue = outcomes[userValue + cpuValue];
